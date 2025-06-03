1afe0dcf9f2164fc0d473206063e1a2e3d5e2a8a
silma <145253957+silmatsania@users.noreply.github.com>
2025-06-03 10:48:31 +0700

    Pertemuan 14 Tugas 1

---
+++ Pertemuan14Tugas1/AppConfig.java
@@ -0,0 +1,7 @@
+package Pertemuan14Tugas1;
+
+public class AppConfig {
+    public static final String BASE_URL = "https://www.myapp.com/api";
+    public static final String APP_VERSION = "1.0.0";
+    public static final int DEFAULT_TIMEOUT = 5000;
+}
---
+++ Pertemuan14Tugas1/TestAppConfig.java
@@ -0,0 +1,9 @@
+package Pertemuan14Tugas1;
+
+public class TestAppConfig {
+    public static void main(String[] args) {
+        System.out.println("BASE_URL: " + AppConfig.BASE_URL);
+        System.out.println("APP_VERSION: " + AppConfig.APP_VERSION);
+        System.out.println("DEFAULT_TIMEOUT: " + AppConfig.DEFAULT_TIMEOUT);
+    }
+}
